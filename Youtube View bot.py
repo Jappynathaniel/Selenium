ba01120c4b1535cab86d3ceb8f280b5677fe6e35
Jasper Nathaniel J <95154891+Jappynathaniel@users.noreply.github.com>
2022-10-04 12:56:08 +0530

    Add files via upload

---
+++ Youtube View bot.py
@@ -0,0 +1,13 @@
+
+from selenium import webdriver 
+import time 
+count = int(input("Enter the number :"))
+ref = int(input("refresh time (in secs):"))
+url=input("Type or Paste the link : ")
+drive = []
+for i in range (count):  
+        drive.append(webdriver.Chrome(r"C:\Users\Jasper Nathaniel J\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\selenium\webdriver\chrome\chromedriver_win32\chromedriver.exe"))
+        drive[i].get(url)
+while True: 
+     time.sleep(ref)
+     drive[i].refresh() 
