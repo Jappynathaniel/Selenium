e663dd2f71a350e5f9ed60b17de5df82555660ca
Jasper Nathaniel J <95154891+Jappynathaniel@users.noreply.github.com>
2022-10-04 13:54:29 +0530

    Update Youtube View bot.py

--- Youtube View bot.py
+++ Youtube View bot.py
@@ -1,4 +1,3 @@
-
 from selenium import webdriver 
 import time 
 count = int(input("Enter the number :"))
@@ -6,7 +5,7 @@ ref = int(input("refresh time (in secs):"))
 url=input("Type or Paste the link : ")
 drive = []
 for i in range (count):  
-        drive.append(webdriver.Chrome(r"C:\Users\Jasper Nathaniel J\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\selenium\webdriver\chrome\chromedriver_win32\chromedriver.exe"))
+        drive.append(webdriver.Chrome(r"C:\Users\Jasper Nathaniel J\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\selenium\webdriver\chrome\chromedriver_win32\chromedriver.exe")) #
         drive[i].get(url)
 while True: 
      time.sleep(ref)
